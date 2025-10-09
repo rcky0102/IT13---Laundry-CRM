01d16142ab20084db456627e578487a282f25ac9
GG-G-M <g.mantilla.545557@umindanao.edu.ph>
2025-10-09 15:03:01 +0800

    init

--- IT13 - Laundry CRM/Repositories/FeedbackRepository.cs
+++ IT13 - Laundry CRM/Repositories/FeedbackRepository.cs
@@ -11,7 +11,7 @@ namespace IT13___Laundry_CRM.Repositories
     internal class FeedbackRepository
     {
         private readonly string connectionString =
-            "Data Source=LAPTOP-NGRORR8P\\SQLEXPRESS;Initial Catalog=LaundryDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            "Data Source=GILGWE\\SQLEXPRESS;Initial Catalog=LaundryDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
 
 
         // CREATE: Add feedback
